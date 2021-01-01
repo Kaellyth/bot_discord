9bf457ef9c55436befec7ad5d75d45012e3f6de2
Kaellyth <hatwock2233@gmail.com>
2021-01-01 16:14:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ client.once('message',(message) => {
     if(message.content === `!ping`){
         message.channel.send('pong');
     }
+    else if(message.content === `!serveur`){
+        message.channel.send(`Nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.membersCount}`);
+    }
+    else if(message.content === `!update`){
+        message.channel.send(`Le Bot est développé par Kaellyt, une sera mis à jour progressivement :blush:`);
+    }
+    else if(message.content === `!point`){
+        if(!message.mentions.users.size){
+            return message.channel.send(`Vous ne pouvez pas vous attribuer personnellement des points`);
+        }
+        const mentionsListe = message.mentions.users.map(user => {
+            return `${user.username} vient de gagner un point.`;
+        });
+
+        message.channel.end(mentionsListe);
+    }
 });
 
 client.login(process.env.TOKEN);
+ 
